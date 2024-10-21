1ac999a7e263ac79d9a214dd5250eb9c5c09619c
your name <you@example.com>
2024-10-21 14:48:35 +0200

    not working YOU FUCKING PIECE OF SHIT! THE contact.js AND contact.html looks the same as index, and your telling me that there is no engine specyfied??? sure, whatever, node and express, kys

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var contactRouter = require('./routes/contact');
+var aboutRouter = require('./routes/about');
+var offerRouter = require('./routes/offer');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -16,6 +19,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contact', contactRouter);
+app.use('/about', aboutRouter);
+app.use('/offer', offerRouter);
 app.use('/static', express.static('static'))
 
 module.exports = app;
