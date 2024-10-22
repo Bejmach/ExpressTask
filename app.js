7ee0443c4cfe92ce28202f3bd1b0e886a7898367
bejmach <bejmach@gmail.com>
2024-10-22 20:50:03 +0200

    cleaning and names to task

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var contactRouter = require('./routes/contact');
-var aboutRouter = require('./routes/about');
-var offerRouter = require('./routes/offer');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,9 +16,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/contact', contactRouter);
-app.use('/about', aboutRouter);
-app.use('/offer', offerRouter);
 app.use('/static', express.static('static'))
 
 module.exports = app;
--- routes/about.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('anout', { title: 'Express' });
-});
-module.exports = router;
